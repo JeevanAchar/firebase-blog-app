91eea86a0f61f13660aaf486eed98d8d09dc6f68
JeevanAchar <jeevanachar83@gmail.com>
2024-05-25 00:45:54 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './components/auth/Login'
 import SignUp from './components/auth/SignUp'
+import Navbar from './components/common/Navbar'
 
 const App = () => {
   return (
     <div>
       {/* <Login/> */}
-      <SignUp />
+      {/* <SignUp /> */}
+      <Navbar />
     </div>
   )
 }
